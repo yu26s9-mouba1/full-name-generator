bd9d085e49c29be24ec87b92803eee57dd38cf25
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-14 11:40:05 -0400

    Initial java File

---
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+
+    }
+
+}
