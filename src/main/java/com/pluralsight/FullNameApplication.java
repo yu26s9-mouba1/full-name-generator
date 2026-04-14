99edcc0fe583cea70fe1c72fd844c9aef0a3115d
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-14 12:52:14 -0400

    Prompted the user for inputs

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -5,7 +5,28 @@ public class FullNameApplication {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Please, enter your first name:");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Please, enter your last name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Please, enter your middle name:");
+        String middleName = scanner.nextLine();
+
+
+        System.out.println("Please, enter your suffix if you have any :");
+        String suffix = scanner.nextLine();
+
+        String fullName = suffix + firstName + middleName + lastName ;
+
         
+
+
+
+
+
+
 
 
     }
